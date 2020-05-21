110cd3b75dc0b65b7e97ef3448c18dd5b78adbed
at-9 <57566734+at-9@users.noreply.github.com>
2020-05-21 23:48:39 +1000

    Update ZigBee-Battery-Accessory-Dimmer.groovy

--- Drivers/ZigBee-Battery-Accessory-Dimmer.groovy
+++ Drivers/ZigBee-Battery-Accessory-Dimmer.groovy
@@ -1,6 +1,6 @@
 /**
  *	Device Driver for Hubitat Elevation
- *    V 0.1
+ *   	V 0.1
  *
  *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *	in compliance with the License. You may obtain a copy of the License at:

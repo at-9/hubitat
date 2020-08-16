9475e22482cdd6718e6db91052008be4ab0d8253
at-9 <57566734+at-9@users.noreply.github.com>
2020-08-16 23:40:32 +1000

    Update Virtual_Siren.groovy

--- Drivers/Virtual_Siren.groovy
+++ Drivers/Virtual_Siren.groovy
@@ -2,7 +2,7 @@
 /**
  *  Virtual Siren
  *	Device Driver for Hubitat Elevation
- *   	V0.1
+ *   	V0.2 Don't code when drunk
  *
  *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *	in compliance with the License. You may obtain a copy of the License at:
